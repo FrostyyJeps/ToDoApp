264a9909658e7c0b46df2e4e58854d08e36d499f
Halfdan <woody7maddox@yahoo.dk>
2020-09-14 10:48:57 +0200

    Todo js almost færdig.

--- js/todo.js
+++ js/todo.js
@@ -1,22 +1,37 @@
 export default class Todo {
 
     constructor() {
+        this.enterButton = document.querySelector('#inputArea button');
+        this.input = document.querySelector('#inputArea input');
+        this.ul = document.querySelector('ul#toDoList');
+
+        this.enterButton.addEventListener('click', (e) => this.addListItem(e));
+        this.input.addEventListener('keypress', (e) => this.addListItem(e));
 
     }
 
-    addListItem(){
-
+    addListItem(e){
+        if(this.input.value.length > 0 && (e.key === 'Enter' || e.key === undefined)){
+            this.createListItem();
+        }
+        
     }
 
     createListItem(){
+        const li = document.createElement('li');
+        li.innerHTML = `${this.input.value} <i class="far fa-trash-alt"></i>`;
+        this.ul.appendChild(li);
+        this.input.value = '';
 
+        li.addEventListener('click', (e) => this.crossOut(e));
+        li.querySelector('i').addEventListener('click', (e) => this.deleteListItem(e))
     }
 
-    crossOut(){
-
+    crossOut(e){
+        e.currentTarget.classList.toggle('done');
     }
 
-    deleteListItem(){
+    deleteListItem(e){
 
     }
 
